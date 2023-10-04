4f4fd6ba82ae91ac72b8dbcb7a9c17d10ff3d07e
Aditya Kasibhatla <dev.kasibhatla@gmail.com>
2023-10-04 17:11:03 +0530

    test forced aspect ratio

--- lib/vap_view.dart
+++ lib/vap_view.dart
@@ -1,17 +1,41 @@
 import 'dart:io';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_vap/vap_view_for_android.dart';
 import 'package:flutter_vap/vap_view_for_ios.dart';
 
 class VapView extends StatelessWidget {
+
+  final double forcedAspectRatio;
+
+  const VapView({Key? key, this.forcedAspectRatio=0}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+
+    Widget child = const SizedBox();
+
     if (Platform.isAndroid) {
-      return VapViewForAndroid();
+      child = Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.red, width: 1),
+        ),
+        child: VapViewForAndroid()
+      );
     } else if (Platform.isIOS) {
-      return VapViewForIos();
+      child = VapViewForIos();
     }
-    return Container();
+
+    // if(forcedAspectRatio>0) {
+    //   return AspectRatio(
+    //     aspectRatio: forcedAspectRatio,
+    //     child: FittedBox(
+    //       fit: BoxFit.fill, //force aspect ratio
+    //       child: child,
+    //     ),
+    //   );
+    // }
+    return child;
   }
 }
